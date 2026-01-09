ac0ae00313e2fb40383257bc94255c9fc442fad3
RAMorales-01 <84475568+RAMorales-01@users.noreply.github.com>
2026-01-09 14:32:35 -0600

    added cases to the MainMenu and Submenu methods for the database options.

--- DatabaseMainEntry.cs
+++ DatabaseMainEntry.cs
@@ -43,10 +43,16 @@ namespace DatabaseMainEntry
 
             switch(selectedOption)
             {
-                case 1: //TODO: EntryManger --- Display entry info
+                case 1: EntryManager.ViewAnEntryVerification();
                 break;
 
-                case 2: //TODO: Go back to main menu
+                case 2: //TODO: EntryManager --- Display Race table 
+                break;
+
+                case 3: //TODO: EntryManager --- Display Job table 
+                break;
+
+                case 4: MainMenuOptions();
                 break;
 
                 default: throw new ArgumentException($"Invalid operation selected", nameof(selectedOption));
